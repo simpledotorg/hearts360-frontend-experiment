1799a0892beaa85c215e8b081a34ae7a5ef733ad
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2025-12-04 18:15:41 +0000

    Better language select button

--- vue3-dashboard/src/components/TheNav.vue
+++ vue3-dashboard/src/components/TheNav.vue
@@ -45,10 +45,19 @@
         <div class="language-selector relative">
           <button 
             @click="showLanguageMenu = !showLanguageMenu"
-            class="bg-gray-600 hover:bg-gray-700 px-4 h-9 flex items-center justify-center rounded-md text-white text-sm"
+            class="bg-gray-600 hover:bg-gray-700 px-4 h-9 flex items-center justify-center gap-2 rounded-md text-white text-sm"
             type="button"
           >
-            {{ currentLanguageLabel }}
+            <span>{{ currentLanguageLabel }}</span>
+            <svg 
+              class="w-4 h-4 transition-transform duration-200"
+              :class="{ 'rotate-180': showLanguageMenu }"
+              xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 20 20" 
+              fill="currentColor"
+            >
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
           </button>
           <div 
             v-if="showLanguageMenu"
